4872ee5fe97c2897f01f9ee00e331b60ebf9ac5a
gunnerr91 <blissvortex@hotmail.com>
2020-09-05 14:45:54 +0100

    added muzzle vfx

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPSCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 20f;
+    [SerializeField] ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -18,16 +20,24 @@ public class Weapon : MonoBehaviour
     {
         if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out var hit, range))
         {
-            Debug.Log("Player ray cast hit with: " + hit.transform.name);
+            PlayMuzzleFlash();
+            ProcessRayCast();
+        }
+    }
 
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRayCast()
+    {
+        if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out var hit, range))
+        {
             if (hit.transform.name == "Enemy" || hit.transform.name == "EnemyHealth")
             {
                 EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
                 enemy.TakeDamage(damage);
-            }
-            else
-            {
-                Debug.Log("name of raycast object: " + hit.transform.name);
             }
         }
     }
