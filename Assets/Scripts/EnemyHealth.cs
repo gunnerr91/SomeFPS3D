4f054ac6675351c8f01c9e3989a8d95ff006ed38
gunnerr91 <blissvortex@hotmail.com>
2020-09-05 14:20:39 +0100

    added raycast and enemy health
    Added a ray cast physics for the weapon, and added enemy health which will reduce when the enemy gets shot

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    public void TakeDamage(float damagePoints)
+    {
+        if(hitPoints > 0)
+        {
+            hitPoints -= damagePoints;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPSCamera;
+    [SerializeField] float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out var hit, range))
+        {
+            Debug.Log("Player ray cast hit with: " + hit.transform.name);
+
+            if (hit.transform.name == "Enemy" || hit.transform.name == "EnemyHealth")
+            {
+                EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
+                enemy.TakeDamage(20f);
+            }
+            else
+            {
+                Debug.Log("name of raycast object: " + hit.transform.name);
+            }
+        }
+    }
+}
