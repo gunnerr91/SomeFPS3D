c204610b075b982af0c7da761904ff95c7720a9a
gunnerr91 <blissvortex@hotmail.com>
2020-09-12 07:49:18 +0100

    updated when enemy will be provoked
    using broadcastmessage to change the provoke stage of the enemy when its taking damage

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -6,6 +6,7 @@ public class EnemyHealth : MonoBehaviour
 
     public void TakeDamage(float damagePoints)
     {
+        BroadcastMessage("OnDamageTaken");
         if(hitPoints > 0)
         {
             hitPoints -= damagePoints;
