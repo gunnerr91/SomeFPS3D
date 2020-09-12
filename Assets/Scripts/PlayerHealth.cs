e7349c20c1d4de25bee7ff09fa7f05e7c07c36d1
gunnerr91 <blissvortex@hotmail.com>
2020-09-12 05:58:26 +0100

    added player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    public void TakeDamage(float damageHitPoints)
+    {
+        hitPoints -= damageHitPoints;
+        if(hitPoints < 1)
+        {
+            Debug.Log("Game over!!");
+        }
+    }
+
+}
