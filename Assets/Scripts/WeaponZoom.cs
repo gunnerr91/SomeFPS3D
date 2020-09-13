c1d5f0569f44ec1d93328ff39c1e49b4fead5b76
gunnerr91 <blissvortex@hotmail.com>
2020-09-13 06:42:15 +0100

    updated player control

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomedOutFov = 60f;
+    [SerializeField] float zoomedInFov = 20f;
+    [SerializeField] float zoomedOutSensitivity = 2f;
+    [SerializeField] float zoomedInSensitivity = 0.5f;
+
+    RigidbodyFirstPersonController fpsController;
+
+    bool zoomedInToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (!zoomedInToggle)
+            {
+                zoomedInToggle = true;
+                fpsController.mouseLook.XSensitivity = zoomedInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedInSensitivity;
+                fpsCamera.fieldOfView = zoomedInFov;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                fpsController.mouseLook.XSensitivity = zoomedOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomedOutSensitivity;
+                fpsCamera.fieldOfView = zoomedOutFov;
+            }
+        }
+    }
+}
